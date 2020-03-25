0700c29e288fe9d3d6387ea49c6f7030d73c3da3
priyanshu3666 <priyanshu.shukla_cs18@gla.ac.in>
2020-03-25 07:04:10 -0700

    final commit
    Signed-off-by: priyanshu3666 <priyanshu.shukla_cs18@gla.ac.in>

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -3,6 +3,7 @@ package problem1.main;
 
 
 import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 
 import java.util.Scanner;
 
@@ -11,13 +12,24 @@ public class MyMain
     public static void main(String[] args)
     {
         MyBinarySearchTree m = new MyBinarySearchTree();
-        m.setRoot(new Scanner(System.in).nextInt());
-        System.out.println("root set" + m.getRoot().getData());
-        for (int i = 0; i < 10; i++) {
+        MyQueue queue=new MyQueue();
+        //setting root
+        m.setRoot();
+        System.out.println("root set : " + m.getRoot().getData());
+
+
+        //insertion
+        for (int i = 0; i <5; i++) {
             m.insert(m.getRoot());
         }
-        m.printLeftChildren();
 
 
+        //printing left children
+        queue.queuePrint(m.getPre());
+        //counting nodes Not having left children
+        m.countNotLeft(m.getRoot());
+        //printing count
+        System.out.println(m.getCount());
     }
+
 }
