bb6c5889ac7c5092ce9b7d5d352d2afa38c5ab7c
Divyansh Bhardwaj <divyanshb22@gmail.com>
2020-03-25 18:35:54 +0530

    initial commit
    Signed-off-by: priyanshu3666 <priyanshu.shukla_cs18@gla.ac.in>

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,23 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
-// executable class
 
-public class MyMain {
+
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        m.setRoot(new Scanner(System.in).nextInt());
+        System.out.println("root set" + m.getRoot().getData());
+        for (int i = 0; i < 10; i++) {
+            m.insert(m.getRoot());
+        }
+        m.printLeftChildren();
+
+
+    }
 }
